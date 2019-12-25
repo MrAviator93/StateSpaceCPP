303df7cb87b1fd35bbe93cd1b621c9e13e9cd504
Artur <kalninsarturs@hotmail.com>
2019-12-25 12:24:38 +0000

    Added additional files.

--- StateSpaceCPlusPlus/UHMath.h
+++ StateSpaceCPlusPlus/UHMath.h
@@ -1,7 +1,7 @@
-/***********************************************************************
+﻿/***********************************************************************
 FILE UHMATH.H CONTAINS BASIC MATRIX CLASS IMPLEMENTATIONS
 
-FILE UTMOST REVIEW DONE ON (23.12.2019) BY ARTUR K. 
+FILE UTMOST REVIEW DONE ON (25.12.2019) BY ARTUR K. 
 ***********************************************************************/
 
 #ifndef UH_MATH_H
@@ -9,10 +9,105 @@ FILE UTMOST REVIEW DONE ON (23.12.2019) BY ARTUR K.
 
 namespace UH
 {
+	//Forward declarations
+	template<class T> class TColumnVec2;
+	template<class T> class TMatrix2x2;
+
+	//-----------------------------------------------------------------------
+	//TEMPLATE ROW VECTOR 2
+	//
+	//Basic Implementation of row vector 2 class.
+	//
+	//  _  _
+	// |x  y|
+	//  ͞   ͞  
+	//-----------------------------------------------------------------------
+
+	template<class T>
+	class TRowVec2
+	{
+	public:
+		TRowVec2()
+		{
+			memset(&m_values, 0 , sizeof(T) * 2);
+		}
+
+		TRowVec2(T value)
+		{
+			m_values[0] = m_values[1] = value;
+		}
+
+		TRowVec2(T a1, T a2)
+		{
+			m_values[0] = a1;
+			m_values[1] = a2;
+		}
+
+		~TRowVec2() = default;
+
+		TRowVec2 operator= (TRowVec2 const& vec2)
+		{
+			m_values[0] = vec2.m_values[0];
+			m_values[1] = vec2.m_values[1];
+			return *this;
+		}
+
+		TRowVec2 operator+ (TRowVec2 const& vec2)
+		{
+			m_values[0] += vec2.m_values[0];
+			m_values[1] += vec2.m_values[1];
+			return *this;
+		}
+
+		TRowVec2 operator* (T const& scalar)
+		{
+			m_values[0] *= scalar;
+			m_values[1] *= scalar;
+			return *this;
+		}
+
+		T operator* (TColumnVec2<T> const& columnVector2)
+		{
+			T scalar = m_values[0] * columnVector2[0] + m_values[1] * columnVector2[1];
+			return scalar;
+		}
+
+		//Access by copy.
+		T operator[](int index) const
+		{
+			if (index >= 0 && index <= 1)
+			{
+				return m_values[index];
+			}
+			//if index out of range return the 0 element
+			return m_values[0];
+		}
+
+		//Access by reference.
+		T& operator[](int index)
+		{
+			if (index >= 0 && index <= 1)
+			{
+				return m_values[index];
+			}
+			//if index out of range return the 0 element
+			return m_values[0];
+		}
+
+	private:
+		T m_values[2];
+	};
+
 	//-----------------------------------------------------------------------
 	//TEMPLATE COLUMN VECTOR 2
 	//
 	//Basic Implementation of column vector 2 class.
+	//
+	// _ _
+	//|	x |
+	//|	  |
+	//|	y |
+	// ͞  ͞
 	//-----------------------------------------------------------------------
 
 	template<class T>
@@ -56,6 +151,16 @@ namespace UH
 			m_values[0] *= scalar;
 			m_values[1] *= scalar;
 			return *this;
+		}
+
+		TMatrix2x2<T> operator* (TRowVec2<T> const& rowVector2)
+		{
+			T a11 = m_values[0] * rowVector2[0];
+			T a12 = m_values[1] * rowVector2[0]; 
+			T a21 = m_values[0] * rowVector2[1];
+			T a22 = m_values[1] * rowVector2[1];
+			TMatrix2x2<T> mat2x2(a11, a12, a21, a22);		
+			return mat2x2;
 		}
 
 		//Access by copy.
@@ -122,10 +227,33 @@ namespace UH
 			return newColumnVec2;
 		}
 
+		//Access by copy.
+		T operator[](int index) const
+		{
+			if (index >= 0 && index <= 3)
+			{
+				return m_values[index];
+			}
+			//if index out of range return the 0 element
+			return m_values[0];
+		}
+
+		//Access by reference.
+		T& operator[](int index)
+		{
+			if (index >= 0 && index <= 3)
+			{
+				return m_values[index];
+			}
+			//if index out of range return the 0 element
+			return m_values[0];
+		}
+
 	private:
 		T m_values[4];
 	};
 
+	using RowVec2 = TRowVec2<double>;
 	using ColumnVec2 = TColumnVec2<double>;
 	using Matrix2x2 = TMatrix2x2<double>;
 
--- StateSpaceCPlusPlus/launcher.cpp
+++ StateSpaceCPlusPlus/launcher.cpp
@@ -1,7 +1,7 @@
 /***********************************************************************
 FILE LAUNCHER.CPP CONTAINS SIMPLE MODEL OF STATE SPACE
 
-FILE UTMOST REVIEW DONE ON (23.12.2019) BY ARTUR K. 
+FILE UTMOST REVIEW DONE ON (25.12.2019) BY ARTUR K. 
 ***********************************************************************/
 
 #include "pch.h"
@@ -9,6 +9,7 @@ FILE UTMOST REVIEW DONE ON (23.12.2019) BY ARTUR K.
 #include <fstream>
 
 #include "UHMath.h"
+#include "Timer/CTimer.h"
 
 using namespace UH;
 
@@ -16,9 +17,13 @@ int main()
 {
     std::cout << "Welcome!\n"; 
 
+	CTimer timer1;
+
 	//State space model
 	Matrix2x2 A(0, 1, -3, -2);
 	ColumnVec2 B(0, 1);
+	RowVec2 C(1, 0);
+	//D matrix doesn't exist in this example D = 0;
 
 	//Initial conditions
 	ColumnVec2 X0(0, 0);
@@ -32,11 +37,14 @@ int main()
 	double simulationTimeS = 160.0; //Simulation time in seconds
 	int n = round((simulationTimeS - initialTime) / dt); //Number of iterations
 
-	//Allocate array.
+	//Allocate arrays
 	ColumnVec2* X = new ColumnVec2[n];
+	double* Y = new double[n];
 
 	//Set initial value
 	X[0] = X0;
+
+	timer1.start();
 
 	//Simulation of the response of the system for the period of simulationTimeS.
 	for (auto i = 1; i < n; i++)
@@ -44,26 +52,37 @@ int main()
 		X[i] = X[i - 1] + (A * X[i - 1] + B * u) * dt;
 	}
 
+	for (auto j = 0; j < n; j++)
+	{
+		//Y is the output
+		Y[j] = C * X[j];
+	}
+
+	auto timeElapsed = timer1.getElapsedTimeUs();
+
 	//Control law
 	//Matrix2x2 Q(1,0,0,2);
 	//double R = 1;
 	//auto K = lqr(A,B,Q,R);
+	// ****** TODO: Implement lqr method.
 
 	//Closed loop system then look like this
 	//ss((A-B*K),B,C,D);
 
 	std::ofstream outFile;
 	outFile.open("outputs.txt");
-	outFile << "Time(s) Column1 Column2";
+	outFile << "Time(s) Column1 Column2 Y";
 	for (auto i = 0; i < n; i++)
 	{
-		outFile << "\n" << i * dt << " " << X[i][0] << " " << X[i][1];
+		outFile << "\n" << i * dt << " " << X[i][0] << " " << X[i][1] << " " << Y[i];
 	}
 	outFile.close();
 
 	//Clean-up after ourselves
 	delete[] X;
+	delete[] Y;
 
 	std::cout << "Done, see output.txt file.\n";
+	std::cout << "Time elapsed: " << timeElapsed << "ms\n";
 	std::cin.get();
 }
